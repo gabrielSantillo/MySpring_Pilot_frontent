39964818a0b902e95b9d4944c3e42edb75ccbe57
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-11-11 15:33:34 -0700

    Desiging my login page

--- src/components/user/LoginUser.vue
+++ src/components/user/LoginUser.vue
@@ -1,25 +1,37 @@
 <template>
-  <div>
-    <v-form ref="form" v-model="valid" lazy-validation>
-      <v-text-field
-        v-model="email"
-        :rules="emailRules"
-        label="E-mail"
-        required
-      ></v-text-field>
+  <div class="container">
+    <img src="https://images.pexels.com/photos/3935702/pexels-photo-3935702.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+    <v-form ref="form" v-model="valid" lazy-validation class="login_form">
+      <div>
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="E-mail"
+          required
+          class="input"
+        ></v-text-field>
 
-      <v-text-field
-        v-model="password"
-        :rules="passwordRules"
-        label="Password"
-        required
-      ></v-text-field>
+        <v-text-field
+          v-model="password"
+          :rules="passwordRules"
+          label="Password"
+          required
+          class="input"
+        ></v-text-field>
+      </div>
 
-      <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
-       Login
-      </v-btn>
+      <div>
+        <v-btn
+          :disabled="!valid"
+          color="success"
+          class="mr-4"
+          @click="validate"
+        >
+          Login
+        </v-btn>
 
-      <v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
+        <v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
+      </div>
     </v-form>
   </div>
 </template>
@@ -29,9 +41,7 @@ export default {
   data: () => ({
     valid: true,
     name: "",
-    passwordRules: [
-      (v) => !!v || "Password is required"
-    ],
+    passwordRules: [(v) => !!v || "Password is required"],
     email: "",
     emailRules: [
       (v) => !!v || "E-mail is required",
@@ -50,4 +60,25 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+    display: grid;
+    grid-template-columns: 4fr 4fr;
+
+    > img {
+    height: 128%;
+    width: 100%;
+    background-size: cover;
+    background-position: center;
+    }
+}
+
+.login_form {
+  display: grid;
+  place-items: center;
+
+  > div > .input {
+    width: 400px;
+  }
+}
+</style>
