bbf15b590f109794b76b60292f73d4b3e84b2d50
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-11-11 11:48:50 -0700

    Making my first component to log in the user

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import LoginPage from '@/views/user/LoginPage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+
+  {
+    path: '/',
+    component: LoginPage
+  }
 
 ]
 
---
+++ src/views/user/LoginPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <login-user></login-user>
+    </div>
+</template>
+
+<script>
+import LoginUser from '@/components/user/LoginUser.vue'
+    export default {
+  components: { LoginUser },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
