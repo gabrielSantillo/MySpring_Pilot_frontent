8ff144ada0f715642277bc44437c68733cdd07eb
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-11-13 21:50:55 -0700

    Made the add request for programs

---
+++ src/components/program/AddProgram.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <h1>Add Program</h1>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field
+        v-model="college_id"
+        :rules="collegetRules"
+        label="College ID"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="program_name"
+        :rules="programRules"
+        label="Program name"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="program_url"
+        :rules="programUrlRules"
+        label="Program URL"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="tuition"
+        :rules="tuitionRules"
+        label="Tuition"
+        required
+      ></v-text-field>
+
+      <v-btn
+        :disabled="!valid"
+        color="success"
+        class="mr-4"
+        @click="post_program"
+      >
+        Add
+      </v-btn>
+
+      <v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+import cookies from "vue-cookies"
+export default {
+  data: () => ({
+    valid: true,
+    college_id: "",
+    appointmentRules: [(v) => !!v || "College ID is required"],
+    program_name: "",
+    programRules: [(v) => !!v || "Program name is required"],
+    program_url: "",
+    programUrlRules: [(v) => !!v || "Program URL is required"],
+    tuition: "",
+    tuitionRules: [(v) => !!v || "Tuition form is required"],
+  }),
+  methods: {
+    post_program() {
+      axios
+        .request({
+          url: `http://127.0.0.1:5000/api/courses`,
+          method: `POST`,
+          headers: {
+            token: `${cookies.get(`token`)}`,
+          },
+          data: {
+            college_id: this.college_id,
+            course_name: this.program_name,
+            course_url: this.program_url,
+            intake: 'fall 2023',
+            tuition: this.tuition,
+          },
+        })
+        .then((response) => {
+          response;
+        })
+        .catch((error) => {
+          error;
+        });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/program/ProgramComponent.vue
+++ src/components/program/ProgramComponent.vue
@@ -7,11 +7,49 @@
       <v-tab @click="college"> College </v-tab>
       <v-tab @click="visa"> Visa </v-tab>
     </v-tabs>
+
+    <section class="program_info">
+      <div v-for="program in programs" :key="program['course_id']">
+        <p><b>Program ID: </b> {{ program["course_id"] }}</p>
+        <p>
+          <b>Program: </b>
+          <a :href="program['course_url']" target="_blank">{{
+            program["course_name"]
+          }}</a>
+        </p>
+        <p><b>Tuition: </b> ${{ program["tuition"] }}</p>
+        <p><b>College ID: </b> {{ program["college_id"] }}</p>
+        <p><b>College Name: </b> {{ program["college_name"] }}</p>
+      </div>
+    </section>
+
+    <section class="edit_program_container">
+      <div>
+        <add-program></add-program>
+      </div>
+
+      <div>
+        <edit-student></edit-student>
+      </div>
+    </section>
+
+    <section class="student_document">
+      <student-document></student-document>
+    </section>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import cookies from "vue-cookies";
+import AddProgram from "./AddProgram.vue";
 export default {
+  components: { AddProgram },
+  data() {
+    return {
+      programs: "",
+    };
+  },
   methods: {
     student() {
       this.$router.push("/student");
@@ -26,10 +64,59 @@ export default {
     },
 
     visa() {
-        this.$router.push('/visa')
-    }
+      this.$router.push("/visa");
+    },
+  },
+
+  mounted() {
+    axios
+      .request({
+        url: `http://127.0.0.1:5000/api/courses`,
+        headers: {
+          token: `${cookies.get("token")}`,
+        },
+      })
+      .then((response) => {
+        this.programs = response["data"];
+      })
+      .catch((error) => {
+        error;
+      });
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.program_info {
+  display: grid;
+  place-items: center;
+  grid-template-columns: repeat(auto-fit, minmax(415px, 1fr));
+  gap: 20px;
+  width: 90%;
+  margin-top: 50px;
+  margin-left: 25px;
+  margin-right: 25px;
+  max-height: 80vh;
+  overflow-y: scroll;
+
+  > div {
+    max-height: 400px;
+    box-shadow: 3px 3px 8px gray;
+    width: 100%;
+    padding: 5px;
+    margin: 5px;
+  }
+}
+
+.edit_program_container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  place-items: center;
+  margin: 100px;
+  column-gap: 100px;
+
+  > div {
+    width: 60%;
+  }
+}
+</style>
