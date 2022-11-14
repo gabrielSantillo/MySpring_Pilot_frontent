fa111fa59628d388798b513ed45aa9e618f08bf6
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-11-13 21:12:27 -0700

    Made the add college component

---
+++ src/components/college/AddCollege.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1>Add College</h1>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field
+        v-model="name"
+        :rules="nameRules"
+        label="Name"
+        required
+      ></v-text-field>
+
+      <v-btn
+        :disabled="!valid"
+        color="success"
+        class="mr-4"
+        @click="post_college"
+      >
+        Add
+      </v-btn>
+
+      <v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+import cookies from "vue-cookies"
+export default {
+  data() {
+    return {
+      valid: true,
+      name: "",
+      nameRules: [(v) => !!v || "Name is required"],
+    };
+  },
+
+  methods: {
+    post_college() {
+      axios
+        .request({
+          url: `http://127.0.0.1:5000/api/college`,
+          method: `POST`,
+          headers: {
+            token: `${cookies.get(`token`)}`,
+          },
+          data: {
+            name: this.name,
+          },
+        })
+        .then((response) => {
+          response;
+        })
+        .catch((error) => {
+          error;
+        });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/college/CollegeComponent.vue
+++ src/components/college/CollegeComponent.vue
@@ -7,11 +7,32 @@
       <v-tab @click="programs"> Programs </v-tab>
       <v-tab @click="visa"> Visa </v-tab>
     </v-tabs>
+
+    <section class="college_info">
+      <div v-for="college in colleges" :key="college['college_id']">
+        <p><b>College ID: </b>{{ college["college_id"] }}</p>
+        <p><b>Name: </b>{{ college["name"] }}</p>
+      </div>
+    </section>
+
+    <section class="edit_college_container">
+      <div>
+        <add-college></add-college>
+      </div>
+
+      <div>
+        <edit-student></edit-student>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import cookies from "vue-cookies";
+import AddCollege from './AddCollege.vue';
 export default {
+  components: { AddCollege },
   methods: {
     student() {
       this.$router.push("/student");
@@ -26,10 +47,62 @@ export default {
     },
 
     visa() {
-        this.$router.push('/visa')
-    }
+      this.$router.push("/visa");
+    },
+  },
+
+  data() {
+    return {
+      colleges: "",
+    };
+  },
+
+  mounted() {
+    axios
+      .request({
+        url: `http://127.0.0.1:5000/api/college`,
+        headers: {
+          token: `${cookies.get("token")}`,
+        },
+      })
+      .then((response) => {
+        this.colleges = response["data"];
+      })
+      .catch((error) => {
+        error;
+      });
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.college_info {
+  display: grid;
+  place-items: center;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  width: 90%;
+  margin-top: 50px;
+  max-height: 80vh;
+  overflow-y: scroll;
+
+  > div {
+    max-height: 400px;
+    box-shadow: 3px 3px 16px gray;
+    width: 70%;
+    margin: 15px;
+    padding: 5px;
+  }
+}
+
+.edit_college_container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  place-items: center;
+  margin: 100px;
+  column-gap: 100px;
+
+  > div {
+    width: 60%;
+  }
+}
+</style>
